f9df5b93a0a19fed53e0ff6162c1a2e0b4d2cfb1
aashut0sh <theofficialaman@gmail.com>
2016-08-21 23:51:41 +0545

    after adding button

---
+++ app/src/main/java/com/aashutoshrestha/dontpanic/activity/SafetyCheckFragment.java
@@ -0,0 +1,46 @@
+package com.aashutoshrestha.dontpanic.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.aashutoshrestha.dontpanic.R;
+
+/**
+ * Created by Aashutosh
+ */
+public class SafetyCheckFragment extends Fragment {
+
+    public SafetyCheckFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_safetycheck, container, false);
+
+
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
