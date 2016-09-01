b76216ac48c07c36914ed4511e63016fb992277d
aashut0sh <theofficialaman@gmail.com>
2016-09-01 21:59:18 +0545

    version0.0.3

--- app/src/main/java/com/aashutoshrestha/dontpanic/activity/MainActivity.java
+++ app/src/main/java/com/aashutoshrestha/dontpanic/activity/MainActivity.java
@@ -61,7 +61,7 @@ public class MainActivity extends AppCompatActivity implements FragmentDrawer.Fr
         }
 
         if(id == R.id.action_search){
-            Toast.makeText(getApplicationContext(), "Search action is selected!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "Flashlight toggled", Toast.LENGTH_SHORT).show();
             return true;
         }
 
