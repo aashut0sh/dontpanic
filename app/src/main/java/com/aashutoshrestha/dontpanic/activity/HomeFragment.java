ce70636632af9bcc7e47e6cda6149466f6473f7e
aashut0sh <theofficialaman@gmail.com>
2016-09-25 17:22:38 +0545

    Password Reset

--- app/src/main/java/com/aashutoshrestha/dontpanic/activity/HomeFragment.java
+++ app/src/main/java/com/aashutoshrestha/dontpanic/activity/HomeFragment.java
@@ -5,19 +5,44 @@ package com.aashutoshrestha.dontpanic.activity;
  */
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.aashutoshrestha.dontpanic.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener{
+
+    //logcat tag
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private final  static int PLAY_SERVICES_RESOLUTION_REQUEST = 1000;
+
+    // Google client to interact with Google API
+    private GoogleApiClient mGoogleApiClient;
+
+
+    // boolean flag to toggle periodic location updates
+    private boolean mRequestingLocationUpdates = false;
+
+   // private LocationRequest mLocationRequest;
+
+    // Location updates intervals in sec
+    private static int UPDATE_INTERVAL = 10000; // 10 sec
+    private static int FATEST_INTERVAL = 5000; // 5 sec
+    private static int DISPLACEMENT = 10; // 10 meters
 
     public HomeFragment() {
         // Required empty public constructor
     }
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,5 +68,20 @@ public class HomeFragment extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
