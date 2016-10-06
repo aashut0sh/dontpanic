b89f8e6ff20946ed0d1886b534b535aa26e5d919
aashut0sh <theofficialaman@gmail.com>
2016-10-06 16:16:51 +0545

    some add feature

---
+++ app/src/main/java/com/aashutoshrestha/dontpanic/activity/incaseOfEmergencyInformation.java
@@ -0,0 +1,13 @@
+package com.aashutoshrestha.dontpanic.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class incaseOfEmergencyInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_incase_of_emergency_information);
+    }
+}
---
+++ app/src/main/java/com/aashutoshrestha/dontpanic/model/UserModel.java
@@ -0,0 +1,8 @@
+package com.aashutoshrestha.dontpanic.model;
+
+/**
+ * Created by aashutosh on 27-Sep-16.
+ */
+
+public class UserModel {
+}
